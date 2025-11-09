b6b3ea052a05e1ddb548401d01d244f9565de020
Taqiyudin <taqiyudinmiftah.01@gmail.com>
2025-11-09 21:49:38 +0700

    Run RAG Pipeline

---
+++ app.py
@@ -0,0 +1,12 @@
+from src.data_loader import load_all_documents
+from src.vectorstore import FaissVectorStore
+
+if __name__ == "__main__":
+    # docs = load_all_documents("data")
+    store = FaissVectorStore("faiss_store")
+    # store.build_from_documents(docs)
+    store.load()
+    print(store.query("What is LangChain?", top_k=3))
+
+
+
